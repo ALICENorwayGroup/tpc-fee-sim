3d612cbb4b2fcb4c8807696d00c3db3198b0c553
Matthias Richter <Matthias.Richter@scieq.net>
2015-07-14 11:02:34 +0200

    various bugfixes

--- generator/ChannelMerger.cxx
+++ generator/ChannelMerger.cxx
@@ -45,6 +45,8 @@ int ChannelMerger::GrowBuffer(unsigned newsize)
   }
   memset(mUnderflowBuffer+mBufferSize, 0, (newsize - mBufferSize) * sizeof(buffer_t));
 
+  mBufferSize=newsize;
+
   return 0;
 }
 
@@ -54,7 +56,8 @@ int ChannelMerger::StartTimeframe()
   //
   buffer_t* lastData=mBuffer;
   mBuffer=mUnderflowBuffer;
-  if (mBuffer) memset(mBuffer, 0, mBufferSize * sizeof(buffer_t));
+  mUnderflowBuffer=lastData;
+  if (mUnderflowBuffer) memset(mUnderflowBuffer, 0, mBufferSize * sizeof(buffer_t));
 
   return 0;
 }
@@ -93,10 +96,10 @@ int ChannelMerger::AddChannel(float offset, unsigned int index, AliAltroRawStrea
     for (Int_t i=0; i<bunchLength; i++) {
       int timebin=startTime-i;
       if (timebin < (int)mChannelLenght && timebin >= 0) {
-	mBuffer[position+timebin]=signals[i];
+	mBuffer[position+timebin]+=signals[i];
       } else if (timebin < 0 && (timebin + (int)mChannelLenght) >= 0) {
 	timebin += mChannelLenght;
-	mUnderflowBuffer[position+timebin]=signals[i];
+	mUnderflowBuffer[position+timebin]+=signals[i];
       } else {
 	// TODO: some out-of-range counter
 	std::cerr << "sample with timebin " << timebin << " out of range" << std::endl;
