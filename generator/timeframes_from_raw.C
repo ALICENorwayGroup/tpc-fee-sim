23e40528d0a73b93a40f1842ee78d4a94aa96043
Matthias Richter <Matthias.Richter@scieq.net>
2015-07-14 12:57:24 +0200

    adding ChannelMerger to macro, finishing basic functionality

--- generator/timeframes_from_raw.C
+++ generator/timeframes_from_raw.C
@@ -17,23 +17,39 @@
   TString macroname=gInterpreter->GetCurrentMacroName();
   macroname+="+";
   gSystem->Load("libGenerator.so");
+  gROOT->LoadMacro("ChannelMerger.cxx+");
   gROOT->LoadMacro(macroname);
   timeframes_from_raw();
 }
 #else
 #include "GeneratorTF.h"
+#include "ChannelMerger.h"
 #include <vector>
 #include <iostream>
 
 const float g_rate=5.;    // rate with respect to unit time, i.e. framesize
+const int   g_nframes=10;    // number of timeframes to be generated
 
 void timeframes_from_raw()
 {
   GeneratorTF generator(g_rate);
-  for (int nTF=0; nTF<10; nTF++) {
+  ChannelMerger merger;
+  int nframes=0;
+
+  while (nframes++<g_nframes || g_nframes<0) {
     const std::vector<float>& tf=generator.SimulateCollisionSequence();
 
-    std::cout << "Timeframe: " << tf.size() << " collisions" << std::endl;
+    int mergedCollisions=merger.MergeCollisions(tf);
+    if (mergedCollisions < 0) {
+      std::cerr << "merging collisions failed with error code " << mergedCollisions << std::endl;
+      break;
+    } else if (mergedCollisions != (int)tf.size()) {
+      // probably no more input data to be read
+      std::cout << "simulated " << nframes-1 << " timeframe(s)" << std::endl;
+      break;
+    }
+
+    std::cout << "Successfully generated timeframe from " << tf.size() << " collisions" << std::endl;
     for (std::vector<float>::const_iterator element=tf.begin(); element!=tf.end(); element++) std::cout << "   collision at offset " << *element << std::endl;
   }
 }
