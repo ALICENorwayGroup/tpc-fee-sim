c1da4d31e3e5fe887b6529b228d352420debcfd9
Matthias Richter <Matthias.Richter@scieq.net>
2015-07-27 14:00:14 +0200

    Setup of Huffman coder and steering of compression from main macro

--- generator/timeframes_from_raw.C
+++ generator/timeframes_from_raw.C
@@ -30,13 +30,48 @@
 #include "TTree.h"
 #include "TFile.h"
 #include "TH1F.h"
+#include "TH2F.h"
 #include "TSystem.h"
+#include "AliHLTHuffman.h"
 
 const float g_rate=5.;    // rate with respect to unit time, i.e. framesize
 const int   g_nframes=1000;    // number of timeframes to be generated
+const int   g_doHuffmanCompression=0; // 0 - off, 1 - compression, 2 - training
+const char* g_huffmanFileName="TPCRawSignalDifference";
+const char* g_targetFileName="tpc-raw-channel-stat.root";
 
 void timeframes_from_raw()
 {
+  // signal bit length
+  const int signalBitLength=10;
+  const int signalRange=0x1<<signalBitLength;
+
+  // huffman decoder settings
+  const char* huffmanDecoderName=g_huffmanFileName;
+  TString htfn=huffmanDecoderName;
+  htfn+="_HuffmanTable.root";
+  AliHLTHuffman* pHuffman=NULL;
+  if (g_doHuffmanCompression==2) {
+    // training mode, create new instance
+    pHuffman=new AliHLTHuffman(huffmanDecoderName, signalBitLength+1);
+  } else if (g_doHuffmanCompression==1) {
+    // compression mode, load huffman table
+    TFile* htf=TFile::Open(htfn);
+    if (!htf || htf->IsZombie()) {
+      cerr << "can not open file " << htfn << endl;
+      cerr << "please run the macro in 'training' mode by setting bRunHuffmanTraining to true" << endl;
+      return;
+    }
+
+    TObject* obj=NULL;
+    htf->GetObject(g_huffmanFileName, obj);
+    if (obj==NULL) {
+      cout << "can not load Huffman decoder object " << huffmanDecoderName << " from file " << htfn << endl;
+      return;
+    }
+    pHuffman=(AliHLTHuffman*)obj;
+  }
+
   GeneratorTF generator(g_rate);
   ChannelMerger merger;
   merger.SetDDLRange(0, 1);
@@ -80,6 +115,7 @@ void timeframes_from_raw()
   int NFilledTimebins=0;
   int NBunches=0;
   int BunchLength[1]; // length is dummy, different variable used for tree filling
+  float HuffmanFactor=1.;
 
   TTree *channelstat=new TTree("channelstat","TPC RAW channel statistics");
   if (channelstat) {
@@ -98,6 +134,40 @@ void timeframes_from_raw()
     channelstat->Branch("NFilledTimebins", &NFilledTimebins , "NFilledTimebins/I");
     channelstat->Branch("NBunches"       , &NBunches        , "NBunches/I");
     channelstat->Branch("BunchLength"    , BunchLength      , "BuncheLength[NBunches]/i");
+  }
+
+  TTree *huffmanstat=NULL;
+
+  if (0)
+    huffmanstat=new TTree("huffmanstat","TPC RAW huffman compression statistics");
+
+  if (huffmanstat) {
+    // huffmanstat->Branch("TimeFrameNo"    , &TimeFrameNo     , "TimeFrameNo/I");
+    // huffmanstat->Branch("DDLNumber"      , &DDLNumber       , "DDLNumber/I");
+    // huffmanstat->Branch("HWAddr"         , &HWAddr          , "HWAddr/I");
+    // huffmanstat->Branch("PadRow"         , &PadRow          , "PadRow/I");
+    huffmanstat->Branch("HuffmanFactor"  , &HuffmanFactor   , "HuffmanFactor/F");
+  }
+
+  TH1* hHuffmanCodeLength=NULL;
+  TH1* hSignalDiff=NULL;
+  TH2* hHuffmanFactor=NULL;
+  if (g_doHuffmanCompression>0) {
+    Int_t binMargin=50; // some margin on both sides of the signal distribution
+    Int_t nBins=2*(signalRange+binMargin)+1;
+    hSignalDiff=new TH1D("hSignalDiff", "Differences in TPC RAW signal", nBins, -nBins/2, nBins/2);
+    hSignalDiff->GetXaxis()->SetTitle("Signal(n+1) - Signal(n)");
+    hSignalDiff->GetYaxis()->SetTitle("counts");
+    hSignalDiff->GetYaxis()->SetTitleOffset(1.4);
+
+    hHuffmanCodeLength=new TH1F("hHuffmanCodeLength", "Huffman code length per signal difference", nBins, -nBins/2, nBins/2);
+    hHuffmanCodeLength->GetXaxis()->SetTitle("Signal(n+1) - Signal(n)");
+    hHuffmanCodeLength->GetYaxis()->SetTitle("Huffman code length");
+    hHuffmanCodeLength->GetYaxis()->SetTitleOffset(1.4);
+
+    hHuffmanFactor=new TH2F("hHuffmanFactor", "Huffman Compression Factor", 61, -1, 60, 100, 0, 5);
+    hHuffmanFactor->GetXaxis()->SetTitle("Padrow number");
+    hHuffmanFactor->GetYaxis()->SetTitle("Huffman compression factor");
   }
 
   std::vector<float> singleTF;
@@ -159,8 +229,12 @@ void timeframes_from_raw()
     // normalization for estimation of baseline
     // not to be used for colision pileup in timeframes
     //merger.Normalize(NCollisions);
-    merger.ApplyZeroSuppression();
-    merger.Analyze(*channelstat);
+    if (g_doHuffmanCompression==0) {
+      merger.ApplyZeroSuppression();
+      merger.Analyze(*channelstat);
+    } else {
+      merger.DoHuffmanCompression(pHuffman, g_doHuffmanCompression==2, *hHuffmanFactor, *hSignalDiff);
+    }
     if (merger.GetSignalOverflowCount() > 0) {
       std::cout << "signal overflow in current timeframe detected" << std::endl;
       bHaveSignalOverflow=true;
@@ -191,10 +265,33 @@ void timeframes_from_raw()
     std::cout << "WARNING: signal overflow detected in at least one timeframe" << std::endl;
   }
 
-  const char* targetFileName="tpc-raw-channel-stat.root";
-  TFile* of=TFile::Open(targetFileName, "RECREATE");
+  if (pHuffman && g_doHuffmanCompression==2) {
+    // training mode, calculate huffman table
+    pHuffman->GenerateHuffmanTree();
+    pHuffman->Print();
+    TFile* htf=TFile::Open(htfn, "RECREATE");
+    if (!htf || htf->IsZombie()) {
+      cerr << "can not open file " << htfn << endl;
+      return;
+    }
+
+    htf->cd();
+    pHuffman->Write();
+    htf->Close();
+  }
+
+  if (pHuffman) {
+    for (int iSignalDiff=-signalRange; iSignalDiff<signalRange; iSignalDiff++) {
+      AliHLTUInt64_t length = 0;
+      AliHLTUInt64_t v = iSignalDiff+signalRange;
+      pHuffman->Encode(v, length);
+      hHuffmanCodeLength->Fill(iSignalDiff, length);
+    }
+  }
+
+  TFile* of=TFile::Open(g_targetFileName, "RECREATE");
   if (!of || of->IsZombie()) {
-    cerr << "can not open file " << targetFileName << endl;
+    cerr << "can not open file " << g_targetFileName << endl;
     return;
   }
 
@@ -211,6 +308,20 @@ void timeframes_from_raw()
 
   if (hCollisionOffset)
     hCollisionOffset->Write();
+
+  if (hHuffmanCodeLength)
+    hHuffmanCodeLength->Write();
+
+  if (hSignalDiff)
+    hSignalDiff->Write();
+
+  if (hHuffmanFactor)
+    hHuffmanFactor->Write();
+
+  if (huffmanstat) {
+    huffmanstat->Print();
+    huffmanstat->Write();
+  }
 
   of->Close();
 }
